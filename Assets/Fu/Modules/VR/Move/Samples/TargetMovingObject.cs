af8a629abb1f915766ce54056637d7c0468b5e87
Lingann <lingann.m@gmail.com>
2019-04-24 18:50:56 +0800

    增加VR模块

---
+++ Assets/Fu/Modules/VR/Move/Samples/TargetMovingObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovingObject : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+
+   [SerializeField] private TargetMoving targetMoving;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            targetMoving.AdjustTransfrom(target);
+        }
+	}
+}
---
+++ Assets/Fu/Modules/VR/Move/TargetMoving.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMoving : MonoBehaviour {
+
+    public Transform freeArea;
+
+    public Transform head;
+
+    public void AdjustPosition(Vector3 targetPos)
+    {
+        Quaternion desiredRotation = Quaternion.LookRotation(head.forward, head.up);
+
+        Quaternion rotationCorrection = desiredRotation * Quaternion.Inverse(head.localRotation);
+
+        freeArea.rotation = rotationCorrection;
+
+        Vector3 positionCorrection = targetPos - head.position;
+
+        freeArea.position = freeArea.position + positionCorrection;
+    }
+
+    public void AdjustRotation(float yEnlerAngles)
+    {
+        transform.rotation = Quaternion.Euler(0, yEnlerAngles - head.localEulerAngles.y, 0);
+    }
+
+    public void AdjustTransfrom(Transform target)
+    {
+        AdjustPosition(target.position);
+
+        AdjustRotation(target.eulerAngles.y);
+
+        AdjustPosition(target.position);
+
+        AdjustRotation(target.eulerAngles.y);
+    }
+
+    void ReSetPosition()
+    {
+        AdjustPosition(Vector3.zero);
+    }
+}
